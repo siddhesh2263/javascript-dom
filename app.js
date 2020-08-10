3a6d85d6ef9216221a367aaee323b98630cce3d1
siddhesh2263 <siddhesh2263@gmail.com>
2020-08-10 14:26:13 +0530

    added custom search filter

--- app.js
+++ app.js
@@ -14,27 +14,32 @@ const addForm = document.forms['add-book'];
 // Prevent empty form submission by using preventDefault()
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    
     const value = addForm.querySelector('input[type="text"]').value;
 
-    // Create elements
-    const li_new = document.createElement('li');
-    const bookName = document.createElement('span');
-    const deleteBtn = document.createElement('span');
+    if(value) {
 
-    // Add context
-    bookName.textContent = value;
-    deleteBtn.textContent = 'delete';
+        // Create elements
+        const li_new = document.createElement('li');
+        const bookName = document.createElement('span');
+        const deleteBtn = document.createElement('span');
 
-    // Add classes
-    bookName.classList.add('name');
-    deleteBtn.classList.add('delete');
-    // You can also use deleteBtn.className = 'delete', but the above implementaion will
-    // allow us to add multiple classes in a correct manner.
+        // Add context
+        bookName.textContent = value;
+        deleteBtn.textContent = 'delete';
 
-    // Append to document
-    li_new.appendChild(bookName);
-    li_new.appendChild(deleteBtn);
-    list.appendChild(li_new);
+        // Add classes
+        bookName.classList.add('name');
+        deleteBtn.classList.add('delete');
+        // You can also use deleteBtn.className = 'delete', but the above implementaion will
+        // allow us to add multiple classes in a correct manner.
+
+        // Append to document
+        li_new.appendChild(bookName);
+        li_new.appendChild(deleteBtn);
+        list.appendChild(li_new);
+
+    }
 
 });
 
@@ -47,4 +52,20 @@ hideBox.addEventListener('change', (e) => {
     } else {
         list.style.display = 'initial';
     }
+});
+
+
+// Filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li'); // This is a HTMLCollection
+    Array.from(books).forEach((book) => {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    });
 });

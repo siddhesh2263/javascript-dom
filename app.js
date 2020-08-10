6a7bc5ac5670e4c9832e8da730b59e94ac938b91
siddhesh2263 <siddhesh2263@gmail.com>
2020-08-10 13:29:56 +0530

    interacting with forms

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const list = document.querySelector('#book-list ul');
 
+// Deleting a book
 list.addEventListener('click', (e) => {
     if(e.target.className == 'delete') {
         const li_target = e.target.parentElement;
         list.removeChild(li_target);
     }
+});
+
+
+const addForm = document.forms['add-book'];
+
+// Prevent empty form submission by using preventDefault()
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 });

af37a4febc345a39719d005162a02a4405b5c5df
siddhesh2263 <siddhesh2263@gmail.com>
2020-08-10 11:11:33 +0530

    changing text and HTML content

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-// console.log(books);
-
 books = document.querySelectorAll('#book-list li .name');
-// console.log(books);
 
-Array.from(books).forEach((book) => {
-    console.log(book);
+// The above line will return a NodeList, and forEach loop can be applied on it
+// without converting it to Array. However a HTMLCollection needs to be converted
+// into an Array.
+
+books.forEach((book) => {
+    console.log(book.textContent);
 });
+
+books.forEach((book) => {
+    book.textContent += ' (book title)';
+});
+
+const bookList = document.querySelector('#book-list');
+// Grabs all the HTML code inside the <div id=book-list> section
+console.log(bookList.innerHTML);
+
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how we add HTML</p>';
--- test-app.js
+++ test-app.js
@@ -17,4 +17,22 @@ lis = Array.from(lis);
 
 lis.forEach((item) => {
     console.log(item);
-})
+});
+
+
+// ====================================================================
+
+
+
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+// console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+// console.log(books);
+
+Array.from(books).forEach((book) => {
+    console.log(book);
+});
